90e7fecbe4ba4af4cb69a2fb89b86baedc61ee58
Diego García <diego.garcia@ceiba.com.co>
2019-08-27 10:36:11 -0500

    Empezaré refactoring

--- Estacionamiento-Ceiba/src/app/components/crear/crear.component.ts
+++ Estacionamiento-Ceiba/src/app/components/crear/crear.component.ts
@@ -1,20 +1,18 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
-import { DialogData } from 'src/app/modals/DialogData';
-import { Vehiculo } from 'src/app/class/vehiculo';
-import { ConexionBDService } from 'src/app/services/conexion-bd.service';
-import Swal from 'sweetalert2';
-import { Router } from '@angular/router';
-
+import { Component, OnInit, Inject } from "@angular/core";
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material";
+import { DialogData } from "src/app/modals/DialogData";
+import { Vehiculo } from "src/app/class/vehiculo";
+import { ConexionBDService } from "src/app/services/conexion-bd.service";
+import Swal from "sweetalert2";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-crear',
-  templateUrl: './crear.component.html',
-  styleUrls: ['./crear.component.css']
+  selector: "app-crear",
+  templateUrl: "./crear.component.html",
+  styleUrls: ["./crear.component.css"]
 })
 export class CrearComponent implements OnInit {
-
-  public vehiculo = new Vehiculo('', '', '', '', 0);
+  public vehiculo = new Vehiculo("", "", "", "", 0);
   public placa: string;
 
   constructor(
@@ -22,7 +20,7 @@ export class CrearComponent implements OnInit {
     @Inject(MAT_DIALOG_DATA) public data: DialogData,
     private conexion: ConexionBDService,
     private router: Router
-  ) { }
+  ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
@@ -31,18 +29,21 @@ export class CrearComponent implements OnInit {
   registrarVehiculo() {
     this.conexion.ingresarVehiculo(this.vehiculo).subscribe(
       data => {
+        console.log(data);
         if (data['estadoOperacion']) {
           this.dialogRef.close();
-          Swal.fire('Vehículo registrado', 'El vehículo puede ingresar. Acceso autorizado!', 'success');
+          Swal.fire(
+            "Vehículo registrado",
+            "El vehículo puede ingresar. Acceso autorizado!",
+            "success"
+          );
         }
       },
       err => {
-        Swal.fire('Error al ingresar el vehículo', err.error.message, 'info');
+        Swal.fire("Error al ingresar el vehículo", err.error.message, "info");
       }
     );
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
